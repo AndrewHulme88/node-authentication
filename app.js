5a8672785df69ca28db3b9c1918c441d4178d561
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-08 09:37:15 +1100

    Updated app.js to setup passport

--- app.js
+++ app.js
@@ -31,4 +31,38 @@ app.post("/sign-up", async (req, res, next) => {
   }
 });
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 app.listen(3000, () => console.log("App listening on port 3000!"));
