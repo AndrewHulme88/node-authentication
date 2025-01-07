11b6ec268709d9e9c8e19c9b7d64f04962eb1f28
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-08 09:53:35 +1100

    Added log out functionality

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("index", { user: req.user }));
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 
 app.post("/sign-up", async (req, res, next) => {
@@ -68,7 +68,16 @@ passport.deserializeUser(async (id, done) => {
 app.post("/log-in", passport.authenticate("local", {
   successRedirect: "/",
   failureRedirect: "/"
-})
+  })
 );
+
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 app.listen(3000, () => console.log("App listening on port 3000!"));
